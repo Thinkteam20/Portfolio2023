bc5bc9bf2dd192de0e1a46a156fe2d31b4cc7877
Sangpil <tankteam20@gmail.com>
2023-01-07 11:32:36 +1100

    set up typescript and dotnet core API

---
+++ data/data.js
@@ -0,0 +1,12 @@
+const data = [
+    {
+        Name: "Angular Speedster Board 2000",
+        Description:
+            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+        Price: 20000,
+        PictureUrl: "/images/products/sb-ang1.png",
+        Brand: "Angular",
+        Type: "Boards",
+        QuantityInStock: 100,
+    },
+];
